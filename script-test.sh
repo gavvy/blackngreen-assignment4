3be9e50ad880f246746d2a39458621fb49b923c1
Cole Gibson <colergibson@gmail.com>
2016-11-02 20:52:37 +0000

    Expanded script to include user input.

--- script-test.sh
+++ script-test.sh
@@ -1,23 +1,87 @@
 #!/bin/bash
+read -p "Which .csv file would you like to use? " filename
 
-timestamp=$(date +"%F_%T")
+if [ -e $filename.csv ];
+then
+    read -p "Would you like to overwrite or append that file? " over_app_var
 
-echo $timestamp > ./example.list
+    until [ $over_app_var == "overwrite" ] || [ $over_app_var == "append" ];
+    do
+        read -p 'Please enter "overwrite" or "append" ' over_app_var
+    done
 
-UniqueID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
+    if [ $over_app_var == "append" ];
+    then
+        timestamp=$(date +"%F_%T")
 
-echo $UniqueID >> ./example.list
+        echo $timestamp > ./$filename.list
 
-read -p "What is your favorite color? " question1_ans
-read -p "Do you prefer mexican food over italian food? " question2_ans 
-read -p "What's your remedy for a sweet tooth? " question3_ans
-read -p "What do you want to do when you grow up? " question4_ans 
-read -p "Where is your dream vacation location? " question5_ans
+        UniqueID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
 
-echo $question1_ans >> ./example.list
-echo $question2_ans >> ./example.list
-echo $question3_ans >> ./example.list
-echo $question4_ans >> ./example.list
-echo $question5_ans >> ./example.list
+        echo $UniqueID >> ./$filename.list
 
-paste -d, -s ./example.list >> ./example.csv
+        read -p "What is your favorite color? " question1_ans
+        read -p "Do you prefer mexican food over italian food? " question2_ans 
+        read -p "What's your remedy for a sweet tooth? " question3_ans
+        read -p "What do you want to do when you grow up? " question4_ans 
+        read -p "Where is your dream vacation location? " question5_ans
+
+        echo $question1_ans >> ./$filename.list
+        echo $question2_ans >> ./$filename.list
+        echo $question3_ans >> ./$filename.list
+        echo $question4_ans >> ./$filename.list
+        echo $question5_ans >> ./$filename.list
+
+        paste -d, -s ./$filename.list >> ./$filename.csv
+    fi
+
+    if [ $over_app_var == "overwrite" ];
+    then
+        timestamp=$(date +"%F_%T")
+
+        echo $timestamp > ./$filename.list
+
+        UniqueID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
+
+        echo $UniqueID >> ./$filename.list
+
+        read -p "What is your favorite color? " question1_ans
+        read -p "Do you prefer mexican food over italian food? " question2_ans 
+        read -p "What's your remedy for a sweet tooth? " question3_ans
+        read -p "What do you want to do when you grow up? " question4_ans 
+        read -p "Where is your dream vacation location? " question5_ans
+
+        echo $question1_ans >> ./$filename.list
+        echo $question2_ans >> ./$filename.list
+        echo $question3_ans >> ./$filename.list
+        echo $question4_ans >> ./$filename.list
+        echo $question5_ans >> ./$filename.list
+
+        paste -d, -s ./$filename.list > ./$filename.csv
+    fi
+fi
+
+if [ ! -e $filename.csv ];
+then
+    timestamp=$(date +"%F_%T")
+
+    echo $timestamp > ./$filename.list
+
+    UniqueID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
+
+    echo $UniqueID >> ./$filename.list
+
+    read -p "What is your favorite color? " question1_ans
+    read -p "Do you prefer mexican food over italian food? " question2_ans 
+    read -p "What's your remedy for a sweet tooth? " question3_ans
+    read -p "What do you want to do when you grow up? " question4_ans 
+    read -p "Where is your dream vacation location? " question5_ans
+
+    echo $question1_ans >> ./$filename.list
+    echo $question2_ans >> ./$filename.list
+    echo $question3_ans >> ./$filename.list
+    echo $question4_ans >> ./$filename.list
+    echo $question5_ans >> ./$filename.list
+
+    paste -d, -s ./$filename.list > ./$filename.csv
+fi
