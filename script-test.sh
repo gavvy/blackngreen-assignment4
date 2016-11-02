1244eac553fa9143377f8e40bc7e3f79ce34f82f
Gavriella <gavvy@live.unc.edu>
2016-11-02 20:51:08 +0000

    bolding

--- script-test.sh
+++ script-test.sh
@@ -8,11 +8,16 @@ UniqueID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
 
 echo $UniqueID >> ./example.list
 
-read -p "What is your favorite color? " question1_ans
-read -p "Do you prefer mexican food over italian food? " question2_ans 
-read -p "What's your remedy for a sweet tooth? " question3_ans
-read -p "What do you want to do when you grow up? " question4_ans 
-read -p "Where is your dream vacation location? " question5_ans
+bold=$(tput bold)
+
+
+
+read -p "${bold}What is your favorite color? "  question1_ans
+read -p "${bold}Do you prefer mexican food over italian food? " question2_ans 
+read -p "${bold}What's your remedy for a sweet tooth? " question3_ans
+read -p "${bold}What do you want to do when you grow up? " question4_ans 
+read -p "${bold}Where is your dream vacation location? " question5_ans
+
 
 echo $question1_ans >> ./example.list
 echo $question2_ans >> ./example.list
